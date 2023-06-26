0572a4bb5b7d7af103d23f3c526ea2f7d0376d7b
Dmytro Karpenko <kardmitriy@gmail.com>
2023-06-26 17:11:03 +0300

    first ts

---
+++ app.ts
@@ -0,0 +1,31 @@
+// const button = document.querySelector('button');
+// const input1 = document.getElementById('num1')! as HTMLInputElement;
+// const input2 = document.getElementById('num2')! as HTMLInputElement;
+
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+
+// button?.addEventListener('click', function () {
+//   console.log(add(+input1.value, +input2.value));
+// });
+
+// ------------------------------------------------------
+
+// Union
+// function combine(params1: string | number, params2: string | number) {
+//   if (typeof params1 === 'string' || typeof params2 === 'string') {
+//     return params1.toString() + params2.toString();
+//   }
+//   return params1 + params2;
+// }
+
+// console.log(combine(1, 5));
+// console.log(combine('str1', 'str2'));
+
+// ---------------------------------------------------------
+
+let active: 'start' | 'end';
+
+active = 'start';
+console.log(active);
